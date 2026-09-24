971d2fdf3b6e12a9a7a5a2a596f071643367668a
Michael Mussato <michimussato@gmail.com>
2026-09-25 01:36:50 +0200

    auto optimization and gc

--- modules/hosts/nixos-qemu/configuration.nix
+++ modules/hosts/nixos-qemu/configuration.nix
@@ -35,6 +35,27 @@
 
     system.stateVersion = "${stateVersion}";
 
+    # To be verified...
+    # OPTIMIZATION
+    # https://nix.dev/manual/nix/2.34/command-ref/conf-file#conf-auto-optimise-store
+    # this slows down builds but hardlinks duplicates
+    nix.auto-optimise-store = true;
+    # https://nixos.wiki/wiki/Storage_optimization
+
+    # GC
+    nix.gc = {
+      automatic = true;
+      # https://www.freedesktop.org/software/systemd/man/latest/systemd.time.html?__goaway_challenge=meta-refresh&__goaway_id=7a61f9b5f162cd8d7bc631c2593fcbad&__goaway_referer=https%3A%2F%2Fsearch.nixos.org%2F
+      dates = "daily";
+      options = "--delete-older-than 30d";
+      persistent = true;
+    };
+
+    nix.extraOptions = ''
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
+    '';
+
   };
 
 }
